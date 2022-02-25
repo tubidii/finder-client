c8735d81bbece141345f01234f348bd89c0e8f5c
canjey <carolnjeri3235@gmail.com>
2022-02-25 21:11:38 +0300

    Location removed

--- pages/location/location.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import {useEffect, useState} from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import {Grid} from "@mui/material";
